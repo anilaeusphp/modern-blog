251f517f44fcb0a87e8dc68288c4f283b1122acf
rubyongainz <123082063+rubyongainz@users.noreply.github.com>
2023-10-08 07:41:49 +0000

    edit form stimulus finished

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -2,7 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="comments"
 export default class extends Controller {
+
+  initialize(){
+    console.log("Comments Controller initialized");
+  }
   connect() {
     console.log("CommentsController Stimulus is mounted");
   }
+
+  toggleForm(e){
+    e.preventDefault();
+    e.stopPropagation();
+    const form = document.getElementById(e.params['form']);
+    const comment = document.getElementById(e.params['body']);
+    form.classList.toggle('d-none');
+    comment.classList.toggle('d-none');
+    
+    }
 }
